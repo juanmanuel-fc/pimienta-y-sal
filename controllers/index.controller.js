1b2099eb21054fb70b45e70bd9a5bd33ae344b44
juanmanuel.fc <juanmanuel.fc@gmail.com>
2022-05-19 03:40:03 -0300

    detalle menu cargado dinamicamente

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,3 +1,29 @@
+const menuItems = [
+	{
+		id: 1,
+		nombre: "Carpaccio fresco",
+		detalle: "Entrada Carpaccio de salmón con cítricos",
+		precio: 65.50,
+	},
+	{
+		id: 2,
+		nombre: "Risotto de berenjena",
+		detalle: "Risotto de berenjena y queso de cabra",
+		precio: 47.00,
+	},
+	{
+		id: 3,
+		nombre: "Mousse de arroz",
+		detalle: "Mousse de arroz con leche y aroma de azahar",
+		precio: 27.50,
+	},
+	{
+		id: 4,
+		nombre: "Espárragos blancos",
+		detalle: " Espárragos blancos con vinagreta de verduras y jamón ibérico",
+		precio: 37.50,
+	},
+];
 
 const indexController = {
 
@@ -6,9 +32,11 @@ const indexController = {
 	},
 
 	detalle: function (req, res) {
-		res.render("detalleMenu");
+		res.render("detalleMenu", {menuItems: menuItems});
 	}
 
 }
 
 module.exports = indexController;
+
+
